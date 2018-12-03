9021c3ace0bb5bda51d6f8c652375d02ba68afc4
LeaK22 <lea.06650@hotmail.fr>
2018-12-03 15:36:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[ ]:
-
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -14,7 +8,7 @@ app = dash.Dash(__name__)
 server = app.server
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
-data = pd.read_csv('nama_10_gdp_1_Data.csv', error_bad_lines = False, engine = 'python', na_values = [':', 'NaN'])
+data = pd.read_csv('nama_10_gdp_1_Data.csv', engine = 'python', na_values = [':', 'NaN'])
 
 eu_values = [
     'European Union (current composition)',
